3164ea5f5ed81593b31834aa785c29e061ce81ae
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-06-27 16:28:02 +0200

    Marquer une tâche comme terminé et supprimer une tâche

--- script.js
+++ script.js
@@ -16,22 +16,60 @@ Structure d'une tâche
 </li>
 */
 
-//Ajouter une nouvelle tâche quand on clique sur "Ajouter"
+index=0
 const btnAjouter = document.querySelector('#btn-ajouter-tache')
 
+//Ajouter une nouvelle tâche quand on clique sur "Ajouter"
 function ajoutTache(){
     const nvelleTache = document.querySelector('#nouvelle-tache')
     const ulTache = document.querySelector('#liste-taches')
 
     const tache = document.createElement('li')    
+    tache.className = 'tache'
     const texteTache = document.createElement('span')
     const btnSupprimer = document.createElement('button')
+    texteTache.classList = 'texte_tache'
+    btnSupprimer.className = 'btn-supprimer'
+    btnSupprimer.innerText = 'Supprimer'
 
     texteTache.innerText = nvelleTache.value
 
     tache.appendChild(texteTache)
     tache.appendChild(btnSupprimer)
     ulTache.appendChild(tache)
+
+    terminerTache()
+    supprimerTache()
+    compteTache()
 }
 
 btnAjouter.addEventListener('click', ajoutTache)
+
+//Marquer une tâche comme terminé quand on clique dessus
+
+function terminerTache() {
+    let ensembleDesTaches = document.querySelectorAll('span')
+    ensembleDesTaches.forEach(element => {
+        element.addEventListener('click', (event) => {
+            event.target.style.textDecoration = 'line-through'
+        })
+    })
+}
+
+//Supprimer une tache avec le bouton supprimer
+function supprimerTache() {
+    const btnSupprimer = document.querySelectorAll('.btn-supprimer')
+    btnSupprimer.forEach(element => {
+        element.addEventListener("click", (event) => {
+            event.target.closest('li').remove()
+        })
+    })
+}
+
+//Bonus : Mettre à jour le compteur de tâches
+let nombreTache = document.getElementById('nombre-taches')
+
+function compteTache(){
+    index++
+    nombreTache.innerText = `${index} tâche(s)`
+}
